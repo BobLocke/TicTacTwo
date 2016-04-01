3ad14ab41df385284a71dd817201fb711c4232f3
BobLocke <lockelocke1620@yahoo.com>
2016-04-01 16:31:43 -0700

    attempted fix for hostgame

--- static/script/socket.js
+++ static/script/socket.js
@@ -1,2 +1,3 @@
 //var socket = io.connect('http://localhost:8080');
-var socket = io.connect('http://172.16.96.3:8080');
+//var socket = io.connect('http://172.16.96.3:8080');
+var socket = io.connect("http://havvy.net:8080");
